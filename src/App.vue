90a44db895fa8511fcde3fb0a64477e6b0391f99
jiangdou <1565133920@qq.com>
2021-09-04 09:37:23 +0800

    项目架构搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    黑马头条
+    <span>黑马头条</span>
     <i class=" toutiao toutiao-shouji"></i>
     <van-button type="primary">主要按钮</van-button>
     <van-button type="info">信息按钮</van-button>
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div class="rem"></div>
   </div>
 </template>
 
@@ -29,5 +30,12 @@ export default {
 </script>
 
 <style scoped>
-
+.rem {
+  width: 1rem;
+  height: 1rem;
+  background-color: red;
+}
+* {
+  font-size: 24px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 import './vant/index.js'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
